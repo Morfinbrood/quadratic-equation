1edf451e8b1e273a94c546147cd5475750c379ac
Yury Staravoitau <MorfinAll@mail.ru>
2018-02-24 09:20:53 +0300

    quadratic-equation/feature: Implemented all task. All tests passed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let str = equation;
+  let a_str = str.substring(0, str.indexOf(" * x^2 "));
+  let b_str = str.substring(str.indexOf(" * x^2 ") + 7, str.indexOf(" * x "));
+  let c_str = str.substring(str.indexOf(" * x ") + 5);
+
+  let a = Number(a_str.replace(/\s+/g, ''));
+  let b = Number(b_str.replace(/\s+/g, ''));
+  let c = Number(c_str.replace(/\s+/g, ''));
+
+  let D = b * b - 4 * a * c;
+
+  let x1 = Math.round((-b + Math.sqrt(D)) / (2 * a));
+
+  let x2 = Math.round((-b - Math.sqrt(D)) / (2 * a));
+
+  if (x1 < x2) {
+    return [x1, x2]
+  }
+  else {
+    return [x2, x1];
+  }
+
 }
